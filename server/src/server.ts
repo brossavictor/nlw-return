87258a3a1fa6e158dcca869140193f3a7ae9d78e
Victor Brossa <brossajoao@gmail.com>
2022-06-09 21:33:57 -0400

    Fixed CORS error.

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,15 +4,11 @@ import { routes } from './routes'
 
 const app = express()
 
-app.use(
-  cors({
-    origin: 'http://localhost:3000'
-  })
-)
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(process.env.PORT || 3333, () => {
+app.listen(3333, () => {
   console.log('HTTPS server running!')
 })
 
