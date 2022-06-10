e7bc1d2805ae8e4e1b8a84b21864dd120da2b07a
Victor Brossa <brossajoao@gmail.com>
2022-06-09 21:25:38 -0400

    Added log error.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -10,21 +10,27 @@ export const routes = express.Router()
 routes.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body
 
-  const prismaFeedbackRepository = new PrismaFeedbackRepository()
-  const nodemailerMailAdapter = new NodemailerMailAdapter()
-  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-    prismaFeedbackRepository,
-    nodemailerMailAdapter
-  )
+  try {
+    const prismaFeedbackRepository = new PrismaFeedbackRepository()
+    const nodemailerMailAdapter = new NodemailerMailAdapter()
+    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+      prismaFeedbackRepository,
+      nodemailerMailAdapter
+    )
 
-  await submitFeedbackUseCase.execute({
-    type,
-    comment,
-    screenshot
-  })
+    await submitFeedbackUseCase.execute({
+      type,
+      comment,
+      screenshot
+    })
 
-  if (typeof req.body.name == 'number') {
-    return res.status(400).json({ error: '' })
+    if (typeof req.body.name == 'number') {
+      return res.status(400).json({ error: '' })
+    }
+    return res.status(201).send()
+  } catch (err) {
+    console.error(err)
+
+    return res.status(500).send()
   }
-  return res.status(201).send()
 })
